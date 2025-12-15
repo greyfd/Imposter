c5a70974d9d87d307aa3ae7a7a851cadc0c108f3
parker-fb <parkerletak@gmail.com>
2025-12-14 19:20:50 -0600

    reveal

--- FinalProject/CardView.swift
+++ FinalProject/CardView.swift
@@ -8,26 +8,28 @@
 import SwiftUI
 
 struct CardView: View {
-    
+
     @State var playerName: String
     @State var isImposter: Bool
     @State var word: String
-    
+
     @State var flip = false
-    
+
     var body: some View {
-        
-        Button{
-            
-            flip.toggle()
-            
-            
-            
+
+        Button {
+
+            withAnimation(.easeInOut(duration: 0.4)) {
+                flip.toggle()
+            }
+
         } label: {
             //Text("\(playerName)")
-            
-            if !flip{
-                Text("\(playerName)")
+
+            ZStack {
+
+           
+                Text(playerName)
                     .foregroundStyle(.white)
                     .font(.title)
                     .padding()
@@ -35,42 +37,33 @@ struct CardView: View {
                         RoundedRectangle(cornerRadius: 10)
                             .foregroundStyle(.blue)
                     )
+                    .opacity(flip ? 0 : 1)
+                    .rotation3DEffect(.degrees(flip ? 180 : 0),axis: (1, 0, 0))
+
+              
+                VStack {
+                    if isImposter {
+                        Text("Imposter")
+                            .foregroundStyle(.red)
+                    } else {
+                        Text(word)
+                            .foregroundStyle(.green)
+                    }
+                }
+                .font(.title)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(.gray)
+                )
+                .opacity(flip ? 1 : 0)
+                .rotation3DEffect(.degrees(flip ? 0 : -180),axis: (1, 0, 0))
                 
                 
             }
-            
-            else{
-                
-                if isImposter{
-                    Text("Imposter")
-                        .foregroundStyle(.red)
-                        .font(.title)
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundStyle(.gray)
-                                .opacity(0.6)
-                        )
-                    
-                }
-                else{
-                    Text("\(word)")
-                        .foregroundStyle(.green)
-                        .font(.title)
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundStyle(.gray)
-                                .opacity(0.6)
-                        )
-                    
-                }
-            }
-            
         }
-        
-        
     }
+
 }
 
 #Preview {
--- FinalProject/RevealView.swift
+++ FinalProject/RevealView.swift
@@ -15,29 +15,81 @@ struct RevealView: View {
     
     @State var cat: String
     
+    @State var playersReady = 0
+    
+    
+    
+    @State var nextView = false
+    
     
     var body: some View {
         
+        NavigationStack{
+        
         VStack{
+            
+            Text("Tap to Reveal Role")
+                .font(.largeTitle)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(.gray)
+                )
+            
+            Text("Don't Let Anyone Else See Your Role!")
+                .font(.subheadline)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(.gray)
+                )
             
             let columns = [GridItem(), GridItem()]
             
-            LazyVGrid(columns: columns) {
+            LazyVGrid(columns: columns, spacing: 50) {
                 ForEach(players){ p in
-                        CardView(playerName: p.name, isImposter: p.isImposter, word: "word")
+                    CardView(playerName: p.name, isImposter: p.isImposter, word: "word")
                 }
-            
+                
             }
+            .padding()
+            
+            
+            Button {
                 
+                playersReady += 1
                 
+                if playersReady == players.count {
+                    
+                    nextView = true
+                    
+                }
                 
-                
+            } label: {
+                Text("Ready Up")
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(.blue)
+                    )
+            }
+            
+            Text("Players Ready: \(playersReady)/\(players.count)")
+                .foregroundStyle(.white)
+                .font(.subheadline)
+            
+            
+            NavigationLink("", destination: GameView(players: players, count: count, cat: cat), isActive: $nextView)
             
             
         }    //vstack
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.black)
         .opacity(0.8)
+        
+    }
         
     }
     
